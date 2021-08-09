a20ba52b70ec81159147b04ec6e72fcbdd90d04d
Gerardo Torres <68238871+gtor23@users.noreply.github.com>
2021-08-09 18:50:22 -0500

    added art collector and rearranged projects

--- src/components/data.js
+++ src/components/data.js
@@ -43,30 +43,38 @@ const Data = () =>{
             description: 'Web arcade application featuring Tic-Tac-Toe and Whac-A-Mole',
             image: "url(/images/arcade.png)"
         },
-        {
-            id: 5,
-            title: 'Calculator',
-            github: 'https://github.com/gtor23/Calculator',
-            url: 'https://eager-roentgen-ca2b94.netlify.app/',
-            description: 'Classic calculator that handles basic math operations ',
-            image: "url(/images/calculator.png)"
-        },
         {   
-            id: 6,
+            id: 5,
             title: 'Stock Height Calculator',
             github: 'https://github.com/gtor23/Stock_Height_Calc',
             description: 'GUI application that calculates the aluminum stock size and required machining parallels',
             image: "url(/images/stock_height_calc.png)"
         },
         {
-            id: 7,
+            id: 6,
             title: 'Tool Search GUI',
             github: 'https://github.com/gtor23/Tool_Search_GUI_App',
             description: 'Desktop application for finding tool information in plant facility',
             image: "url(/images/tool_search_gui.png)"
         },
         {
+            id: 7,
+            title: 'Art Collector',
+            github: 'https://github.com/gtor23/Art_Collector',
+            url: 'https://vigilant-feynman-f63d82.netlify.app/',
+            description: 'Search engine for The Harvard Art Museums',
+            image: "url(/images/art_collector.png)"
+        },
+        {
             id: 8,
+            title: 'Calculator',
+            github: 'https://github.com/gtor23/Calculator',
+            url: 'https://eager-roentgen-ca2b94.netlify.app/',
+            description: 'Classic calculator that handles basic math operations',
+            image: "url(/images/calculator.png)"
+        },
+        {
+            id: 9,
             title: 'Retro Draw',
             github: 'https://github.com/gtor23/Retro_Draw',
             url: 'https://compassionate-wright-7d3fd5.netlify.app/',
